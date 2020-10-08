20835f5bbcb38374b2fa64200337935006278e78
Art <d.art.shishkin@gmail.com>
2020-10-08 16:38:15 +0300

    22 Build Library Event Domain

---
+++ library-events-producer/src/main/java/com/artarkatesoft/learnkafka/libraryeventsproducer/domain/Book.java
@@ -0,0 +1,16 @@
+package com.artarkatesoft.learnkafka.libraryeventsproducer.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Book {
+    private Integer id;
+    private String name;
+    private String author;
+}
