09ad4ce0ec9861bb88dc4513876302b579d74193
Art <d.art.shishkin@gmail.com>
2020-10-08 20:12:38 +0300

    28_29 Build LibraryEvents Producer using KafkaTemplate

--- library-events-producer/src/main/java/com/artarkatesoft/learnkafka/libraryeventsproducer/controllers/LibraryEventsController.java
+++ library-events-producer/src/main/java/com/artarkatesoft/learnkafka/libraryeventsproducer/controllers/LibraryEventsController.java
@@ -1,20 +1,25 @@
 package com.artarkatesoft.learnkafka.libraryeventsproducer.controllers;
 
 import com.artarkatesoft.learnkafka.libraryeventsproducer.domain.LibraryEvent;
+import com.artarkatesoft.learnkafka.libraryeventsproducer.producers.LibraryEventProducer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(LibraryEventsController.BASE_URL)
 public class LibraryEventsController {
 
     static final String BASE_URL = "/v1/libraryevents";
 
+    private final LibraryEventProducer libraryEventProducer;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public LibraryEvent newLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
-
-//invoke kafka producer
+    public LibraryEvent newLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
+        libraryEventProducer.sendLibraryEvent(libraryEvent);
         return libraryEvent;
     }
 }
---
+++ library-events-producer/src/main/java/com/artarkatesoft/learnkafka/libraryeventsproducer/producers/LibraryEventProducer.java
@@ -0,0 +1,39 @@
+package com.artarkatesoft.learnkafka.libraryeventsproducer.producers;
+
+import com.artarkatesoft.learnkafka.libraryeventsproducer.domain.LibraryEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class LibraryEventProducer {
+
+    private final KafkaTemplate<Integer, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+    public void sendLibraryEvent(LibraryEvent libraryEvent) throws JsonProcessingException {
+        Integer key = libraryEvent.getLibraryEventId();
+        String jsonValue = objectMapper.writeValueAsString(libraryEvent);
+        ListenableFuture<SendResult<Integer, String>> future = kafkaTemplate.sendDefault(key, jsonValue);
+        future.addCallback(
+                result -> handleSuccess(key, jsonValue, result),
+                ex -> handleFailure(key, jsonValue, ex)
+        );
+    }
+
+    private void handleFailure(Integer key, String value, Throwable ex) {
+        log.error("Exception while sending message the key: {} and the value is {}", key, value, ex);
+    }
+
+    private void handleSuccess(Integer key, String value, SendResult<Integer, String> result) {
+        log.info("Message sent successfully for the key: {} and the value is {}, partition is {}", key, value, result.getRecordMetadata().partition());
+    }
+
+}
