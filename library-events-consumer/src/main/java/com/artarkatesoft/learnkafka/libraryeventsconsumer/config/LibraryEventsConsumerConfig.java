676c937f6ad01948cf0f549e4bee9e405a0bdfcd
Art <d.art.shishkin@gmail.com>
2020-10-10 22:59:11 +0300

    57 Concurrent Consumers

--- library-events-consumer/src/main/java/com/artarkatesoft/learnkafka/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
+++ library-events-consumer/src/main/java/com/artarkatesoft/learnkafka/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
@@ -9,7 +9,6 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ContainerProperties;
 
 @Configuration
 @EnableKafka
@@ -24,10 +23,9 @@ public class LibraryEventsConsumerConfig {
         ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         configurer.configure(factory, kafkaConsumerFactory
                 .getIfAvailable(() -> new DefaultKafkaConsumerFactory<>(properties.buildConsumerProperties())));
-//        configurer.configure(factory, kafkaConsumerFactory
-//                .getIfAvailable(() -> new DefaultKafkaConsumerFactory<>(this.properties.buildConsumerProperties())));
 
-        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+//        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        factory.setConcurrency(3);
         return factory;
     }
 
--- library-events-consumer/src/main/java/com/artarkatesoft/learnkafka/libraryeventsconsumer/consumers/LibraryEventsConsumer.java
+++ library-events-consumer/src/main/java/com/artarkatesoft/learnkafka/libraryeventsconsumer/consumers/LibraryEventsConsumer.java
@@ -3,9 +3,10 @@ package com.artarkatesoft.learnkafka.libraryeventsconsumer.consumers;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
 @Slf4j
-//@Component
+@Component
 public class LibraryEventsConsumer {
 
     static final String TOPIC = "library-events";
