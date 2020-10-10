c135e407d8ac21eb6094248405cfe8e4edceb91b
Art <d.art.shishkin@gmail.com>
2020-10-10 22:52:33 +0300

    56 Manual Consumer Offset Management

--- library-events-consumer/src/main/java/com/artarkatesoft/learnkafka/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
+++ library-events-consumer/src/main/java/com/artarkatesoft/learnkafka/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
@@ -1,9 +1,34 @@
 package com.artarkatesoft.learnkafka.libraryeventsconsumer.config;
 
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 
 @Configuration
 @EnableKafka
 public class LibraryEventsConsumerConfig {
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory,
+            KafkaProperties properties) {
+
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, kafkaConsumerFactory
+                .getIfAvailable(() -> new DefaultKafkaConsumerFactory<>(properties.buildConsumerProperties())));
+//        configurer.configure(factory, kafkaConsumerFactory
+//                .getIfAvailable(() -> new DefaultKafkaConsumerFactory<>(this.properties.buildConsumerProperties())));
+
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        return factory;
+    }
+
 }
