5faef5d3f31a9c1c29281f9d6dccec5afc523e88
Art <d.art.shishkin@gmail.com>
2020-10-10 15:58:21 +0300

    51 Configure Kafka Consumer Using Spring Boot Profiles

---
+++ library-events-consumer/src/main/java/com/artarkatesoft/learnkafka/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,9 @@
+package com.artarkatesoft.learnkafka.libraryeventsconsumer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class LibraryEventsConsumerConfig {
+}
