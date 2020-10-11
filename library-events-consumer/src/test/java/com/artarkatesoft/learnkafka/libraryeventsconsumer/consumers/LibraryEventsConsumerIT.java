1b27c204259357827151b907d11611d4e133697b
admin <d.art.shishkin@gmail.com>
2020-10-11 21:51:23 +0300

    62 Configure Embedded Kafka for Integration Tests

---
+++ library-events-consumer/src/test/java/com/artarkatesoft/learnkafka/libraryeventsconsumer/consumers/LibraryEventsConsumerIT.java
@@ -0,0 +1,42 @@
+package com.artarkatesoft.learnkafka.libraryeventsconsumer.consumers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.MessageListenerContainer;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {
+        "spring.kafka.consumer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+        "spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}"
+})
+@EmbeddedKafka(topics = {"library-events"}, partitions = 3)
+class LibraryEventsConsumerIT {
+
+    @Autowired
+    EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Autowired
+    KafkaTemplate<Integer, String> kafkaTemplate;
+
+    @Autowired
+    KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
+
+    @BeforeEach
+    void setUp() {
+        for (MessageListenerContainer listenerContainer : kafkaListenerEndpointRegistry.getAllListenerContainers()) {
+            ContainerTestUtils.waitForAssignment(listenerContainer, embeddedKafkaBroker.getPartitionsPerTopic());
+        }
+    }
+}
