30cefc2514534b38fd6e0efd2e33d17dbf5ed35c
admin <d.art.shishkin@gmail.com>
2020-10-11 22:58:59 +0300

    64 Write Integration Test for UPDATING LibraryEvent

--- library-events-consumer/src/test/java/com/artarkatesoft/learnkafka/libraryeventsconsumer/consumers/LibraryEventsConsumerIT.java
+++ library-events-consumer/src/test/java/com/artarkatesoft/learnkafka/libraryeventsconsumer/consumers/LibraryEventsConsumerIT.java
@@ -1,10 +1,13 @@
 package com.artarkatesoft.learnkafka.libraryeventsconsumer.consumers;
 
+import com.artarkatesoft.learnkafka.libraryeventsconsumer.entity.Book;
+import com.artarkatesoft.learnkafka.libraryeventsconsumer.entity.LibraryEvent;
 import com.artarkatesoft.learnkafka.libraryeventsconsumer.repositories.LibraryEventsRepository;
 import com.artarkatesoft.learnkafka.libraryeventsconsumer.services.LibraryEventsService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,12 +40,15 @@ import static org.mockito.BDDMockito.then;
 @EmbeddedKafka(topics = {"library-events"}, partitions = 3)
 class LibraryEventsConsumerIT {
 
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     EmbeddedKafkaBroker embeddedKafkaBroker;
 
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     KafkaTemplate<Integer, String> kafkaTemplate;
 
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
 
@@ -57,10 +63,14 @@ class LibraryEventsConsumerIT {
 
     @BeforeEach
     void setUp() {
-        repository.deleteAll();
         for (MessageListenerContainer listenerContainer : kafkaListenerEndpointRegistry.getAllListenerContainers()) {
             ContainerTestUtils.waitForAssignment(listenerContainer, embeddedKafkaBroker.getPartitionsPerTopic());
         }
+    }
+
+    @AfterEach
+    void tearDown() {
+        repository.deleteAll();
     }
 
     @Test
@@ -84,5 +94,40 @@ class LibraryEventsConsumerIT {
                 () -> assertThat(libraryEvent.getBook().getName()).isEqualTo("Kafka Using Spring Boot"),
                 () -> assertThat(libraryEvent.getBook().getAuthor()).isEqualTo("Art")
         ));
+    }
+
+    @Test
+    void publishUpdateLibraryEvent() throws ExecutionException, InterruptedException, JsonProcessingException {
+        //given
+        Book book = Book.builder()
+                .id(123)
+                .author("Art")
+                .name("We are the best")
+                .build();
+        LibraryEvent libraryEventToBeSavedInDB = LibraryEvent.builder().book(book).build();
+        book.setLibraryEvent(libraryEventToBeSavedInDB);
+
+        LibraryEvent savedEvent = repository.save(libraryEventToBeSavedInDB);
+        Integer libraryEventId = savedEvent.getLibraryEventId();
+
+        String json = "{\"libraryEventId\":" + libraryEventId + ",\"libraryEventType\":\"UPDATE\",\"book\":{\"id\":123,\"name\":\"We are TESTING Kafka\",\"author\":\"NoArt\"}}";
+
+        //when
+        kafkaTemplate.sendDefault(libraryEventId, json).get();
+
+        CountDownLatch latch = new CountDownLatch(1);
+        latch.await(3, TimeUnit.SECONDS);
+
+        //then
+        then(libraryEventsConsumerSpy).should().onMessage(isA(ConsumerRecord.class));
+        then(libraryEventsServiceSpy).should().processLibraryEvents(isA(ConsumerRecord.class));
+
+        assertThat(repository.count()).isEqualTo(1);
+        LibraryEvent libraryEventUpdated = repository.findById(libraryEventId).get();
+        assertAll(
+                () -> assertThat(libraryEventUpdated.getBook().getId()).isEqualTo(123),
+                () -> assertThat(libraryEventUpdated.getBook().getName()).isEqualTo("We are TESTING Kafka"),
+                () -> assertThat(libraryEventUpdated.getBook().getAuthor()).isEqualTo("NoArt")
+        );
     }
 }
